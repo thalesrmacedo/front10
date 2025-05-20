b82ba8e51c4ec25e1c7d0d7ddbb2ee0521cd12e7
thalesrmacedo <165821269+thalesrmacedo@users.noreply.github.com>
2025-05-19 21:15:55 -0300

    Create contadorSlice.js

---
+++ contadorSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const contadorSlice = createSlice({
+  name: 'contador',
+  initialState: {
+    valor: 0,
+  },
+  reducers: {
+    incrementar: (state) => {
+      state.valor += 1;
+    },
+    decrementar: (state) => {
+      state.valor -= 1;
+    }
+  }
+});
+
+export const { incrementar, decrementar } = contadorSlice.actions;
+export default contadorSlice.reducer;
