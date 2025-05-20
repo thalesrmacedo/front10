7a5675c1df6fd6e8c30c741c8aa800de3dad2e8e
thalesrmacedo <165821269+thalesrmacedo@users.noreply.github.com>
2025-05-19 21:16:35 -0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Contador from './features/contador/contador';
+
+function App() {
+  return (
+    <div className="App">
+      <Contador />
+    </div>
+  );
+}
+
+export default App;
